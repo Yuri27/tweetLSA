023dee5f3d2eec4648982b6f78b6063e7840ca03
DESKTOP-68JV87I\yurin <yuri.nagotnyuk@gmail.com>
2018-06-01 02:06:51 +0300

    Edit file ttwitter.php

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -14,8 +14,9 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
+	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : 'GVSs3YF89Ky45gaIrkjevzA78',
+	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') 'aGsRZeRmic5Hq82Mkq0USlK2KBerlry3pT65G42gngkpQL0a0x',
+	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '2940135034-Q7VEh3aQBZ8HNOKpnvms92uu0Xr1tywUC9S3z8U',
+	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') :'LpWhlvbVjaaZmCL0cEQ4SPPjduHTP8xavqnVh2rX5KGUb',
+	
 ];
